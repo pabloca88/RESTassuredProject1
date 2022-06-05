b461bbdc6f8918a66a67421ccfcc47c691e4089b
pabloca88 <pabloca88@hotmail.com>
2022-06-04 22:33:50 -0300

    size of items dislayed working

---
+++ src/test/java/test/UserTest.java
@@ -0,0 +1,96 @@
+package test;
+
+import domain.User;
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class UserTest extends BaseTest {
+
+    private static final String LIST_USERS_ENDPOINT = "/users";
+    private static final String CREATE_USER_ENDPOINT = "/user";
+    private static final String SHOW_USER_ENDPOINT = "/users/{userId}";
+
+    @Test
+    public void testSpecificPageIsDisplayed() {
+        given().
+                param("pages","2").
+        when().
+                get(LIST_USERS_ENDPOINT).
+        then().
+                statusCode(HttpStatus.SC_OK).
+                body("pages", is(2)).
+                body("data", is(notNullValue()));
+    }
+
+    @Test
+    public void testAbleToCreateNewUser() {
+        Map<String, String> user = new HashMap<>();
+        user.put("nombre", "rafael");
+        user.put("trabajo", "eng test");
+
+        given().
+                body(user).
+        when().
+                post(CREATE_USER_ENDPOINT).
+        then().
+                statusCode(HttpStatus.SC_CREATED).
+                body("name", is("rafael"));
+    }
+
+    @Test
+    public void testSizeOfItemsDisplayedAreTheSameAsPerPage() {
+        int expectedPage = 2;
+
+        int expectedItemsPerPage = getExpectedItemsPerPage(expectedPage);
+
+        given().
+                param("page",expectedPage).
+        when().
+                get(LIST_USERS_ENDPOINT).
+        then().
+                statusCode(HttpStatus.SC_OK).
+                body(
+                        "page", is(expectedPage),
+                        "data.size()", is(expectedItemsPerPage ),
+                        "data.findAll { it.avatar.startsWith('https://reqres.in') }.size()", is(expectedItemsPerPage)
+                );
+    }
+
+    @Test
+    public void testShowSpecificUser() {
+        User user =
+            given().
+                pathParam("userId", 2).
+            when().
+                get(SHOW_USER_ENDPOINT).
+            then().
+                statusCode(HttpStatus.SC_OK).
+                extract().
+                body().jsonPath().getObject("data", User.class);
+
+
+        assertThat(user.getEmail(), containsString("@reqres.in"));
+        assertThat(user.getName(), containsString("Janet"));
+        assertThat(user.getLastName(), containsString("Weaver"));
+    }
+
+    private int getExpectedItemsPerPage(int page) {
+        return given().
+                    param("page", page).
+                when().
+                    get(LIST_USERS_ENDPOINT).
+                then().
+                    statusCode(HttpStatus.SC_OK).
+                    extract().
+                    path("per_page");
+    }
+}
